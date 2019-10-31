fff09be28332ce1421bae039237540ad3fb1b453
darlon.carvalho <darlon.carvalho@hotmail.com>
2019-10-31 11:22:14 -0300

    simples hello Wold com spring boot

---
+++ HelloWord/src/main/java/br/com/devmmidia/HelloWord/HelloWordApplication.java
@@ -0,0 +1,19 @@
+package br.com.devmmidia.HelloWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordApplication.class, args);
+
+		System.out.println("teste de desenvolvimento");
+
+
+	}
+}
+
+
+
